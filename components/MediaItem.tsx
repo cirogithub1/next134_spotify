5c4f5fcf344b72ba0ce5e6dc4bc42749fa432ae3
cirogithub1 <cirosequea.fr@gmail.com>
2023-06-10 00:52:33 +0200

    100623
    getting data from supabase

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -3,6 +3,7 @@
 import { FC } from "react"
 import { Song } from "@/types"
 import useLoadImage from "@/hooks/useLoadImage"
+import Image from "next/image"
 
 interface Props {
 	data: Song
@@ -21,8 +22,32 @@ const MediaItem: FC<Props> = ({ data , onClick }) => {
 	}
 
 	return (
-		<div>
-			{data.title}
+		<div
+			className="
+				flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
+			onClick={handleClick}>
+				<div 
+					className="
+						relative rounded-md min-h-[47px] min-w-[47px] overflow-hidden">
+							<Image 
+								src={imageUrl || '/images/liked.png'}
+								alt={imageUrl || '/images/liked.png'}
+								className="object-cover"
+								fill />
+
+				</div>
+
+				<div 
+					className="
+						flex flex-col gap-y-1 overflow-hidden">
+							<p className="text-white trumcate">
+								{data.title}
+							</p>
+
+							<p className="text-neutral-400 text-sm truncate">
+								{data.author}
+							</p>
+				</div>
 		</div>
 	)
 }
